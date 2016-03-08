806010a48bb7325c185f38d8f9a8b1a267b5f5c3
Asim Hasan <asim.hasan@duke.edu>
2016-03-07 20:03:32 -0500

    Added incomplete tasks counter

--- simple-todos.js
+++ simple-todos.js
@@ -1,11 +1,25 @@
 Tasks = new Mongo.Collection("tasks");
 
 if (Meteor.isClient) {
+  // This code only runs on the client
   Template.body.helpers({
     tasks: function () {
-      return Tasks.find({}, {sort: {createdAt: -1}});
+      if (Session.get("hideCompleted")) {
+        // If hide completed is checked, filter tasks
+        return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
+      } else {
+        // Otherwise, return all of the tasks
+        return Tasks.find({}, {sort: {createdAt: -1}});
+      }
+    },
+    hideCompleted: function () {
+      return Session.get("hideCompleted");
+    },
+    incompleteCount: function () {
+      return Tasks.find({checked: {$ne: true}}).count();
     }
   });
+
   Template.body.events({
     "submit .new-task": function (event) {
       // Prevent default browser form submit
@@ -23,6 +37,9 @@ if (Meteor.isClient) {
 
       // Clear form
       event.target.text.value = "";
+    },
+    "change .hide-completed input": function (event) {
+      Session.set("hideCompleted", event.target.checked);
     }
   });
 
